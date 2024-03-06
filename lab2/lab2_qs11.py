69ab0d2d9cb2c7b4ede0781689dbd63c437b5cfd
KetsuneH <keywhobp@gmail.com>
2024-03-07 11:02:48 +1300

    lab3 initiated

--- lab2/lab2_qs11.py
+++ lab2/lab2_qs11.py
@@ -1,14 +1,10 @@
 import math
 per_drink_volume_ml = 200
-per_container_drinks = 65
-per_container_volume_ml = per_drink_volume_ml * per_container_drinks
+drinks_per_container = 65
 
+while True:
+    number_of_people = int(input("Enter the number of people: "))
+    drinks_wasted = (drinks_per_container - number_of_people) % drinks_per_container
+    volume_wasted_ml = drinks_wasted * per_drink_volume_ml
 
-number_of_people = int(input("Enter the number of people: "))
-total_volume_required = number_of_people * per_drink_volume_ml
-total_containers_required = math.ceil(total_volume_required / per_container_volume_ml)
-
-volume_wasted_ml = (total_containers_required * per_container_volume_ml) - total_volume_required
-drinks_wasted = round(volume_wasted_ml/per_drink_volume_ml)
-
-print(drinks_wasted, "drinks ({}ml) are wasted.".format(volume_wasted_ml))
+    print(drinks_wasted, "drinks ({}ml) are wasted.".format(volume_wasted_ml))
