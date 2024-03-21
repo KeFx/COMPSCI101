cafc1aed2d7a86808b33fb4e7b1884d909cf1d3a
KetsuneH <keywhobp@gmail.com>
2024-03-21 16:06:58 +1300

    shorter and more efficient version of lab6_qs10 finished

---
+++ lab6/lab6_old_qs10.py
@@ -0,0 +1,26 @@
+expression = list(input("Enter an expression: "))
+pairs_counter = 0
+
+def tell_if_character_is_in_list_and_remove_first_one_seen(char, list_) :
+    for character in list_ :
+        if character == char :
+            expression[list_.index(character)] = " "
+            return True
+    return False
+
+index = 0
+
+while index < len(expression) :
+    if expression[index] == "(" :
+        if tell_if_character_is_in_list_and_remove_first_one_seen(")", expression) :
+            expression[index] = " "
+            pairs_counter += 1
+    elif expression[index] == ")":
+        expression[index] = "invalid"
+    index += 1
+
+if ("invalid" in expression) or ("(" in expression) :
+    print("Incorrect.")
+else :
+    print("Correct.")
+    print(f"There are {pairs_counter} pairs of parentheses.")
--- lab6/lab6_qs10.py
+++ lab6/lab6_qs10.py
@@ -1,26 +1,19 @@
 expression = list(input("Enter an expression: "))
 pairs_counter = 0
+invalid_closing_bracket = False
+num_of_open_brackets = 0
 
-def tell_if_character_is_in_list_and_remove_first_one_seen(char, list_) :
-    for character in list_ :
-        if character == char :
-            expression[list_.index(character)] = " "
-            return True
-    return False
+for char in expression :
+    if char == "(":
+        num_of_open_brackets += 1
+    elif char == ')' and num_of_open_brackets:
+        pairs_counter += 1
+        num_of_open_brackets -= 1
+    elif char == ')':
+        invalid_closing_bracket = True
 
-index = 0
-while index < len(expression) :
-    if expression[index] == "(" :
-        if tell_if_character_is_in_list_and_remove_first_one_seen(")", expression) :
-            expression[index] = " "
-            pairs_counter += 1
-    elif expression[index] == ")":
-        expression[index] = "invalid"
-    index += 1
-
-if ("invalid" in expression) or ("(" in expression) :
-    print("Incorrect.")
+if invalid_closing_bracket or num_of_open_brackets:   
+    print("Incorrect.") 
 else :
     print("Correct.")
     print(f"There are {pairs_counter} pairs of parentheses.")
-        
