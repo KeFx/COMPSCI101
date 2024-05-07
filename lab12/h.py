a8086b5056d2ca0f0a00f4c8edcbcba770cfcbc0
KetsuneH <keywhobp@gmail.com>
2024-05-07 13:00:36 +1200

    changed 12_7 to better algorithm

--- lab12/h.py
+++ lab12/h.py
@@ -1,23 +1,24 @@
 def get_duplicates_count(numbers_list):
     numbers_list.sort()
-    number_count = []
+    print(numbers_list)
+    number_count = {}
 
     previous = 'x'
     count  = 0
-    for i in numbers_list:
-        
+    
+    for index, i in enumerate(numbers_list):
         if i == previous:
             count += 1
         else:
             if (count > 1) :
-                number_count.append((previous, count))
+                number_count[previous] = count
             previous = i
             count = 1
-            
-    if (count > 1) :
-        number_count.append((i, count))
 
-    return number_count
+    if (count > 1) :
+        number_count[previous] = count
+
+    print(number_count)
             
 # numbers = [1]
 numbers = [9, 5, 1, 4, 5, 1, 7, 7, 3, 2, 8, 1, 9, 4, 1, 2, 3, 9, 7, 6]
